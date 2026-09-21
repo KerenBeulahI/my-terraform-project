b307605dfe18f53bb4961aa0d4dbff69a1ce19e8
kerenbeulah <kerenbeulah21@gmail.com>
2026-09-21 20:38:37 +0530

    Initial Terraform project: S3 bucket

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_first_bucket" {
+  bucket = "keren-first-terraform-bucket-12345"
+}
